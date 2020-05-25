fd3126ea54d06cb28b05b0b9f3f3a37aa43bc426
talalzeini <56060993+talalzeini@users.noreply.github.com>
2020-05-24 22:43:13 -0700

    first commit

---
+++ Assets/Scripts/gameManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class gameManager : MonoBehaviour
+{
+  public void TryAgain(){
+       SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+   }
+   public void MenuToGame(){
+       SceneManager.LoadScene("Game");
+   }
+    public void GameToMenu(){
+       SceneManager.LoadScene("Menu");
+   }
+}
---
+++ Assets/Scripts/playerScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerScript : MonoBehaviour
+{
+    public GameObject losePanel;
+    public GameObject winPanel;
+    public GameObject gameTitle;
+    public Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        losePanel.SetActive(false);
+        winPanel.SetActive(false);
+        gameTitle.SetActive(true);
+    }
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.DownArrow)){
+        rb.isKinematic = false;
+        }
+    }
+void OnCollisionEnter2D(Collision2D col){
+    if(col.gameObject.CompareTag("Corona")){
+    Invoke("DestroyedObject", 0.6f);
+    losePanel.SetActive(true);
+    winPanel.SetActive(false);
+    gameTitle.SetActive(false);
+    }else if(col.gameObject.CompareTag("Finish")){
+       winPanel.SetActive(true);
+       losePanel.SetActive(false);
+       gameTitle.SetActive(false);
+    }
+}
+void DestroyedObject(){
+    Destroy(gameObject);
+}
+}
